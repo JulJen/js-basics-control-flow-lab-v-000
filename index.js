107715a20c5b7049995355a527db0c2d9939267f
Jul Jen <juliajen.prog@gmail.com>
2018-09-01 00:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,14 +6,10 @@ function scuberGreetingForFeet(distance) {
   } else if (distance > 2500) {
     ride = 'No can do.';
   } else {
-  ride = 'I will gladly take your thirty bucks.';}
-  return ride
-}
+    ride = 'I will gladly take your thirty bucks.';}
+    return ride
+  }
 
 function ternaryCheckCity(destination) {
-  let response;
-  if (!!destination = 'NYC') {
-    response = 'Ok, sounds good.';
-  } else {
-    response = 'No go.';
+  return destination = 'NYC' ? 'Ok, sounds good.' : 'No go.';}
 }
