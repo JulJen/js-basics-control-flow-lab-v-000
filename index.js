8030d748d24d0d143d6abe985f718b1fe68315c4
Jul Jen <juliajen.prog@gmail.com>
2018-09-01 00:32:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ function scuberGreetingForFeet(distance) {
   } else if (distance > 2500) {
     ride = 'No can do.';
   } else {
-    ride = 'I will gladly take your thirty bucks.';}
-    return ride
+    ride = 'I will gladly take your thirty bucks.';
   }
+  return ride
+}
 
 function ternaryCheckCity(destination) {
   return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.'
