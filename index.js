44bf6fbbeb78131bb7ab4fa2a65cb86461fb7ec4
Jul Jen <juliajen.prog@gmail.com>
2018-09-01 00:20:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,17 +9,3 @@ function scuberGreetingForFeet(distance) {
   ride = 'I will gladly take your thirty bucks.';}
   return ride
 }
-
-
-// * const mood = 'happy';
-// * let response;
-// * switch (mood) {
-// *   case 'happy':
-// *     response = 'Heck yea; be happy!';
-// *   case 'sad':
-// *     response = "You're awesome; keep your head up!";
-// *   default:
-// *     response = "Sorry, I don't know how to respond to that mood.";
-// * }
-// * // => "Sorry, I don't know how to respond to that mood."
-// *  response;  // => "Sorry, I don't know how to respond to that mood."
