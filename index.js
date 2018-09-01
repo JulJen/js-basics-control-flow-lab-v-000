ad473e7c8c8eb84d6f3ef7d589f911893c751bfc
Jul Jen <juliajen.prog@gmail.com>
2018-09-01 00:14:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let ride
+  if (distance) <= 400 {
+    ride  = 'This one is on me!';
+  }
+  return result
+}
+
+
+// * const mood = 'happy';
+// * let response;
+// * switch (mood) {
+// *   case 'happy':
+// *     response = 'Heck yea; be happy!';
+// *   case 'sad':
+// *     response = "You're awesome; keep your head up!";
+// *   default:
+// *     response = "Sorry, I don't know how to respond to that mood.";
+// * }
+// * // => "Sorry, I don't know how to respond to that mood."
+// *  response;  // => "Sorry, I don't know how to respond to that mood."
